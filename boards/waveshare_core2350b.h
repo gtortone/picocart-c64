b053ba369a846d839dd3b7d7819f7b4444b6f642
Gennaro Tortone <gtortone@gmail.com>
2026-03-14 18:20:46 +0100

    add boards selection with cmake

---
+++ boards/waveshare_core2350b.h
@@ -0,0 +1,26 @@
+#ifndef WAVESHARE_CORE2350B_BOARD
+#define WAVESHARE_CORE2350B_BOARD
+
+pico_board_cmake_set(PICO_PLATFORM, rp2350)
+
+#define RASPBERRYPI_PICO2
+
+#define PICO_RP2350A 0
+
+#define PICO_BOOT_STAGE2_CHOOSE_W25Q080 1
+
+#ifndef PICO_FLASH_SPI_CLKDIV
+#define PICO_FLASH_SPI_CLKDIV 2
+#endif
+
+pico_board_cmake_set_default(PICO_FLASH_SIZE_BYTES, (16 * 1024 * 1024))
+#ifndef PICO_FLASH_SIZE_BYTES
+#define PICO_FLASH_SIZE_BYTES (16 * 1024 * 1024)
+#endif
+
+pico_board_cmake_set_default(PICO_RP2350_A2_SUPPORTED, 1)
+#ifndef PICO_RP2350_A2_SUPPORTED
+#define PICO_RP2350_A2_SUPPORTED 1
+#endif
+
+#endif
